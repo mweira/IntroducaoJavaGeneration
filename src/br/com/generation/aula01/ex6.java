9127f343c8711a88dcc352cd4926f15751919735
João Pedro <jpmeira020@gmail.com>
2022-04-04 12:07:15 -0300

    Commit 2

---
+++ src/br/com/generation/aula01/ex6.java
@@ -0,0 +1,33 @@
+package br.com.generation.aula01;
+
+import java.util.Scanner;
+
+public class ex6 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		int x1, y1, x2, y2; 
+		double D;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite um valor para x1 : ");
+		x1 = leia.nextInt();
+		
+		System.out.println("Digite um valor para y1 : ");
+		y1 = leia.nextInt();
+		
+		System.out.println("Digite um valor para x2 : ");
+		x2 = leia.nextInt();
+		
+		System.out.println("Digite um valor para y2 : ");
+		y2 = leia.nextInt();
+		
+		D = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		
+		System.out.println("O valor de D será : " + D);
+		
+		Thread.sleep(10000);
+		
+	}
+}
